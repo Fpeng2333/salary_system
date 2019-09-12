40894b4e750cce0f83a51c6fc33b282948a4b89b
fang <pengfup6@gmail.com>
2019-09-12 17:02:31 +0800

    salary_system

---
+++ src/main/java/com/yf/salary/controller/DefaultHtmlController.java
@@ -0,0 +1,23 @@
+package com.yf.salary.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class DefaultHtmlController implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index.html").setViewName("index");
+        registry.addViewController("/login").setViewName("index");
+        registry.addViewController("/platform/**").setViewName("index");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX + "/static/");
+    }
+}
